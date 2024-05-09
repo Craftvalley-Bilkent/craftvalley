a86dd64add0c6e4004f2d2e47421d2e3f6473870
Eren Aslan <erennaslan03@gmail.com>
2024-05-09 22:23:46 +0300

    Database backend Conenction
    Image retrieval and proto-product detail implemented. There will be improvements.

--- craftvalley/user/views.py
+++ craftvalley/user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db import connection
 from django.http import HttpResponse
+from django.db import connection
+import base64
 
 
 # Create your views here.
@@ -50,22 +52,60 @@ def add_product(request):
         return redirect('product_list')
     else:
         return render(request, 'user/add_product.html')
+#TEST
+def delete_all_products():
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM Product")
+#TEST
+
+
 def showProducts(request):
-    all_products = [
-        {'name': f'Product {i}', 'description': f'Description for Product {i}', 'price': i * 10}
-        for i in range(1, 101)  
+    #TEST
+    image_path_1 = 'first_product_image.jpg'
+    image_path_2 = 'second_product_image.jpg'
+
+    with open(image_path_1, 'rb') as image_file_1:
+        image_data_1 = image_file_1.read()
+
+    with open(image_path_2, 'rb') as image_file_2:
+        image_data_2 = image_file_2.read()
+
+    delete_all_products()
+    product_data = [
+        (5, 'First Product', 'Very good product', 12.5, 3, image_data_1),
+        (6, 'Second Product', 'Bad product >:(', 250, 74, image_data_2)
     ]
+    with connection.cursor() as cursor:
+        sql_query = "INSERT INTO Product(product_id, title, description, price, amount, images) VALUES (%s, %s, %s, %s, %s, %s)"
+
+        for data in product_data:
+            cursor.execute(sql_query, data)
+    #TEST
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Product")
+        rows = cursor.fetchall()
     
-    # Pagination logic
+    all_products = []
+    for row in rows:
+        product = {
+            'product_id': row[0],
+            'title': row[1],
+            'description': row[2],
+            'price': row[3],
+            'amount': row[4],
+            'images': base64.b64encode(row[5]).decode() if row[5] else None,
+        }
+        all_products.append(product)
+    
     per_page = 16
     total_products = len(all_products)
-    total_pages = (total_products + per_page - 1) // per_page  # Calculate total pages
+    total_pages = (total_products + per_page - 1) // per_page 
     current_page = int(request.GET.get('page', 1))
     start_index = max(0, (current_page - 1) * per_page)
     end_index = min(start_index + per_page, total_products)
     products = all_products[start_index:end_index]
 
-    # Generate page range for pagination links
     page_range = range(max(1, current_page - 2), min(total_pages + 1, current_page + 3))
 
     return render(request, 'user/mainPageUser.html', {'products': products, 'page_range': page_range, 'current_page': current_page, 'total_pages': total_pages})
