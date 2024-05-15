4cc84356f0cbb27152007fe470a9f10c9a96b255
Ege Sirvan <egesirvan2003@outlook.com>
2024-05-15 16:09:11 +0300

    some changes

--- craftvalley/user/urls.py
+++ craftvalley/user/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", user.views.login),
     path('add_product/', user.views.add_product, name='add_product'),
     path("main", user.views.showProducts),
+    path("cart", user.views.showCart),
 ]
