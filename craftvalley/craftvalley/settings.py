68f8c49b8e87777dd03a415bfa53197248c8f983
elisturegun <elisturegun8@gmail.com>
2024-05-18 00:33:22 +0300

    settings updated

--- craftvalley/craftvalley/settings.py
+++ craftvalley/craftvalley/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = "craftvalley.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ "templates" ],
+        #"DIRS": [ "templates" ],
+        'DIRS': [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -121,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+#STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
