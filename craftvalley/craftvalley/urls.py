bb546216eaba4199c8b5f4c15bce7c93a9634aef
ksarp <ksarpersahin@gmail.com>
2024-05-18 16:47:08 +0300

    Add auth

--- craftvalley/craftvalley/urls.py
+++ craftvalley/craftvalley/urls.py
@@ -21,5 +21,6 @@ import user.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", include(user.urls.urlpatterns)),   
+    path("login/", include(user.urls.urlpatterns)),
+    path('auth/', include('authentication.urls')),   
 ]
