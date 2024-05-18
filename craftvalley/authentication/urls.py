8501e5ac4f5169d0a414ef4e49f8850d11597ea2
ksarp <ksarpersahin@gmail.com>
2024-05-18 22:03:54 +0300

    small_business_profiles edit funtion added

--- craftvalley/authentication/urls.py
+++ craftvalley/authentication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register_business/', views.register_business, name='register_business'),
     path('login/', views.login_view, name='login'),
     path('small_business_profile/', views.small_business_profile, name='small_business_profile'),
+    path('edit_business_profile/', views.edit_business_profile, name='edit_business_profile'),
 ]
